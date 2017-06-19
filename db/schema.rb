7fd31e6ffb54e01b865c70c4cf32f3e6f89e5d9b
Nephtali Rodriguez <nrodriguez@xogrp.com>
2017-06-19 18:04:12 -0400

    UPdates to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170619195735) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "groomsmen", force: :cascade do |t|
     t.string "name"
     t.string "password"
