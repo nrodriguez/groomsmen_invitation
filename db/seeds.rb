276d77241818e24ad769f63efcf97f9162b1cd87
Nephtali Rodriguez <nrodriguez@xogrp.com>
2017-06-20 15:37:13 -0400

    Updating questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,141 +16,96 @@ groomsmen = {
   lewis: 'sinepz'
 }
 
-# groomsmen.each do |key, value|
-#   Groomsman.create(name: key, password: value)
-# end
+groomsmen.each do |key, value|
+  Groomsman.create(name: key, password: value)
+end
 
-
-josh = Groomsman.find_by_name("josh")
+# Josh
+josh = Groomsman.find_by_name('josh')
 QuestionAnswer.create(
   question: "What was Impact's finishing move in BSW? (HINT: It was ICE COLD)",
-  answer: "Impact Icer",
+  answer: 'Impact Icer',
+  groomsman: josh
+)
+QuestionAnswer.create(
+  question: 'My nemesis in Smash Bros in Stony Brook',
+  answer: 'Jared',
+  groomsman: josh
+)
+QuestionAnswer.create(
+  question: 'Who was the famous ref of Enforcer vs Impact (The Backyard Wars)',
+  answer: 'Gabriel',
+  groomsman: josh
+)
+QuestionAnswer.create(
+  question: 'Alex got into a fight with him at my birthday party in the apartment. What was his karaoke name?',
+  answer: 'Mama Joe',
+  groomsman: josh
+)
+QuestionAnswer.create(
+  question: 'We made her cry on the phone when Jason, you and I called her out on her lies.',
+  answer: 'Screg',
   groomsman: josh
 )
 
-josh_questions = [
-  QuestionAnswer.create(
-    question: "Your first main in Super Smash Bros?",
-    answer: "Captain Falcon"
-  ),
-  QuestionAnswer.create(
-    question: "Who was the famous ref of Enforcer vs Impact (The Backyard Wars)",
-    answer: "Gabriel",
-  )
-  QuestionAnswer.create(
-    question: "Whose house did we play Karaoke Revolution at after High School?",
-    answer: "Gabe",
-  )
-  QuestionAnswer.create(
-    question: "", answer: ""}
-]
+# Eric
+eric = Groomsman.find_by_name('eric')
+QuestionAnswer.create(
+  question: 'What move did I do off of the slide to you to win the WFA title in 2006?',
+  answer: "Enforcer's Edge",
+  groomsman: eric
+)
+QuestionAnswer.create(
+  question: 'What other band/artist was our bands to go to when our girlfriends were pissing us off? (A Day to Remember and ???)',
+  answer: 'Drake',
+  groomsman: eric
+)
+QuestionAnswer.create(
+  question: 'The go-to crew drink that we consumed at your house using a funnel',
+  answer: '99 gangstas',
+  groomsman: eric
+)
+QuestionAnswer.create(
+  question: 'What restaurant did we eat at in Queens where we hit on the waitress?',
+  answer: 'Chilis',
+  groomsman: eric
+)
+QuestionAnswer.create(
+  question: 'What did I always say was my favorite move you would do in WFA?',
+  answer: 'Leg Drop',
+  groomsman: eric
+)
 
-ahmed_questions = [
-    QuestionAnswer.create(
-    question: "", answer1: ""
-  ),
-  QuestionAnswer.create(
-    question
-     "", answer: ""},
-  QuestionAnswer.create(
-    question
-     "", answer: ""},
-  QuestionAnswer.create(
-    question
-     "", answer: ""},
-  QuestionAnswer.create(
-    question
-     "", answer: ""}
-]
+# Jesse
+jesse = Groomsman.find_by_name('jesse')
+QuestionAnswer.create(
+  question: 'What movie inspired my gimmick used in our feud in the final year of WFA?',
+  answer: "Saw",
+  groomsman: jesse
+)
+QuestionAnswer.create(
+  question: 'In our very first feud, I gave you a lump on the head with this move on a chair. What was it?',
+  answer: 'Tombstone Piledriver',
+  groomsman: jesse
+)
+QuestionAnswer.create(
+  question: '',
+  answer: '',
+  groomsman: jesse
+)
+QuestionAnswer.create(
+  question: '',
+  answer: '',
+  groomsman: jesse
+)
+QuestionAnswer.create(
+  question: '',
+  answer: '',
+  groomsman: jesse
+)
 
-eric_questions = [
-    QuestionAnswer.create(
-    question: "", answer1: ""},
-  QuestionAnswer.create(
-    question
-     "", answer: ""},
-  QuestionAnswer.create(
-    question
-     "", answer: ""},
-  QuestionAnswer.create(
-    question
-     "", answer: ""},
-  QuestionAnswer.create(
-    question
-     "", answer: ""}
-]
-
-jesse_questions = [
-    QuestionAnswer.create(
-    question: "", answer1: ""},
-  QuestionAnswer.create(
-    question
-     "", answer: ""},
-  QuestionAnswer.create(
-    question
-     "", answer: ""},
-  QuestionAnswer.create(
-    question
-     "", answer: ""},
-  QuestionAnswer.create(
-    question
-     "", answer: ""}
-]
-
-jason_questions = [
-    QuestionAnswer.create(
-    question: "", answer1: ""},
-  QuestionAnswer.create(
-    question
-     "", answer: ""},
-  QuestionAnswer.create(
-    question
-     "", answer: ""},
-  QuestionAnswer.create(
-    question
-     "", answer: ""},
-  QuestionAnswer.create(
-    question
-     "", answer: ""}
-]
-
-juan_questions = [
-    QuestionAnswer.create(
-    question: "", answer1: ""},
-  QuestionAnswer.create(
-    question
-     "", answer: ""},
-  QuestionAnswer.create(
-    question
-     "", answer: ""},
-  QuestionAnswer.create(
-    question
-     "", answer: ""},
-  QuestionAnswer.create(
-    question
-     "", answer: ""}
-]
-
-lewis_questions = [
-    QuestionAnswer.create(
-    question: "", answer1: ""},
-  QuestionAnswer.create(
-    question
-     "", answer: ""},
-  QuestionAnswer.create(
-    question
-     "", answer: ""},
-  QuestionAnswer.create(
-    question
-     "", answer: ""},
-  QuestionAnswer.create(
-    question
-     "", answer: ""}
-]
-
-# require "
-yebug"
-# byebug
-# groomsmen.each do |key, value|
-#   Groomsman.create(name: key, password: value)
-# end
+# QuestionAnswer.create(
+#   question: "Whose house did we play Karaoke Revolution at after High School?",
+#   answer: "Gabe",
+#   groomsman: lewis
+# )
